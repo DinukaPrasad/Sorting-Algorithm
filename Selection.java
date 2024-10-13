d0fd9b51adcbd29280485727d2a648280702c7fe
Dinuka_prasad <127348943+DinukaPrasad@users.noreply.github.com>
2024-10-13 20:25:54 +0530

    Update Selection.java

--- Selection.java
+++ Selection.java
@@ -1,17 +1,8 @@
 package Sorting;
 
 public class Selection {
-    public static void main(String[] args) {
 
-        int arr[]={5,1,6,7,2,8};
-
-        selection(arr);
-
-        print(arr);
-   
-    }
-
-    public static void selection(int arr[]){
+    public void selection(int arr[]){
         
         int in,out,min;
 
@@ -30,13 +21,13 @@ public class Selection {
         }
     }
 
-    public static void print(int arr[]){
+    public void print(int arr[]){
         for(int i=0; i<arr.length;i++){
             System.out.println(i+".Index - "+arr[i]);
         }
     }
 
-    public static void swap(int x,int y,int[]arr){
+    public void swap(int x,int y,int[]arr){
         int temp;
 
         temp = arr[x];
